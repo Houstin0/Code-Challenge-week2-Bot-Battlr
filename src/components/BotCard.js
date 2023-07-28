6fb133bc715acf320fd36aaafb59f96b66739bc2
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-28 14:16:24 +0300

    added all available details to bot card

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,9 +1,15 @@
 function BotCard({bot}) {
     return (
         <div>
+            <img src={bot.avatar_url} alt={bot.name}/>
             <h3>{bot.name}</h3>
             <p>Class: {bot.bot_class}</p>
-            <img src={bot.avatar_url} alt={bot.name}/>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Catchphrase: {bot.catchphrase}</p>
+            <p>Created at: {bot.created_at}</p>
+            <p>Updated at: {bot.updated_at}</p>
         </div>
     )
 }
